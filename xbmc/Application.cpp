d56686acabaaccb5809bde7fdf99321661281cda
Garrett Brown <garbearucla@gmail.com>
2014-03-12 18:44:25 -0700

    Compiler fixes

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -1541,7 +1541,7 @@ bool CApplication::Initialize()
   CJoystickManager::Get().SetEnabled(CSettings::Get().GetBool("input.enablejoystick") &&
                     CPeripheralImon::GetCountOfImonsConflictWithDInput() == 0);
 
-  GAMES::CGameManager::Get().Start();
+  GAME::CGameManager::Get().Start();
 
   return true;
 }
@@ -3499,7 +3499,7 @@ void CApplication::Stop(int exitCode)
     m_pPlayer->ClosePlayer();
 
     StopPVRManager();
-    GAMES::CGameManager::Get().Stop();
+    GAME::CGameManager::Get().Stop();
     StopServices();
     //Sleep(5000);
 
