623effdb1511e0be2a5e3e1a05023cb4bf40e270
Tuxity <kevin.darcel@gmail.com>
2014-05-03 15:52:40 +0200

    Fix merge error and change if condition similar to others

--- xbmc/interfaces/Builtins.cpp
+++ xbmc/interfaces/Builtins.cpp
@@ -574,16 +574,6 @@ int CBuiltins::Execute(const CStdString& execString)
           // Otherwise there are 2 entries for the same plugin in ViewModesX.db
           urlParameters = "/";
         }
-        else if (addon->Type() >= ADDON_SCRIPT && addon->Type() <= ADDON_SCRIPT_LYRICS)
-          // Pass the script name (params[0]) and all the parameters
-          // (params[1] ... params[x]) separated by a comma to RunScript
-          cmd = StringUtils::Format("RunScript(%s)", StringUtils::JoinString(params, ",").c_str());
-        else if (addon->Type() == ADDON_GAMEDLL && params.size() >= 2)
-        {
-          CFileItem item(params[1], false);
-          item.SetProperty("gameclient", params[0]);
-          return g_application.PlayMedia(item);
-        }
 
         if (plugin->Provides(CPluginSource::VIDEO))
           cmd = StringUtils::Format("ActivateWindow(Videos,plugin://%s%s,return)", addonid.c_str(), urlParameters.c_str());
@@ -604,6 +594,12 @@ int CBuiltins::Execute(const CStdString& execString)
         // Pass the script name (params[0]) and all the parameters
         // (params[1] ... params[x]) separated by a comma to RunScript
         cmd = StringUtils::Format("RunScript(%s)", StringUtils::JoinString(params, ",").c_str());
+      else if (CAddonMgr::Get().GetAddon(params[0], addon, ADDON_GAMEDLL) && params.size() >= 2)
+      {
+          CFileItem item(params[1], false);
+          item.SetProperty("gameclient", params[0]);
+          return g_application.PlayMedia(item);
+      }
 
       return Execute(cmd);
     }
